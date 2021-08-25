a7c28ea14395517f214c0b610ebfa1a49b1dc400
luciano <lucholeyria@gmail.com>
2021-08-25 08:19:40 -0300

    Creacion de un nuevo componente para filtrar los expenses por año

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,26 @@
 import ExpenseItem from "./ExpenseItem/ExpenseItem";
 import Card from "../Card/Card";
+import FilterExpense from "./FilterExpense/FilterExpense";
 
 import classes from "./Expenses.module.css";
+import { Fragment } from "react";
 
 const Expenses = (props) => {
    const expenses = props.expensesList;
 
+   const filterByYear = (year) => {
+      console.log(year);
+   };
+
    return (
-      <Card className={classes.expenses}>
-         {expenses.map((expense) => {
-            return <ExpenseItem data={expense} key={expense.id} />;
-         })}
-      </Card>
+      <Fragment>
+         <Card className={classes.expenses}>
+            <FilterExpense filterByYear={filterByYear} />
+            {expenses.map((expense) => {
+               return <ExpenseItem data={expense} key={expense.id} />;
+            })}
+         </Card>
+      </Fragment>
    );
 };
 
---
+++ src/components/Expenses/FilterExpense/FilterExpense.js
@@ -0,0 +1,22 @@
+import classes from "./FilterExpense.module.css";
+
+const FilterExpense = (props) => {
+   const onChangeHandler = (e) => {
+      props.filterByYear(e.target.value);
+   };
+
+   return (
+      <div className={classes["expenses-filter"]}>
+         <div className={classes["expenses-filter__control"]}>
+            <label>Date</label>
+            <select onChange={onChangeHandler}>
+               <option value="2021">2021</option>
+               <option value="2020">2020</option>
+               <option value="2019">2019</option>
+            </select>
+         </div>
+      </div>
+   );
+};
+
+export default FilterExpense;
